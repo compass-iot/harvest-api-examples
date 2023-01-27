6ae7ddc2ad4b04434e660e990c36ebec621c4ec6
david <david@compassiot.com.au>
2023-01-27 16:43:09 +1100

    add examples for batch and partial apis

--- streaming/csharp/ExampleClient.cs
+++ streaming/csharp/ExampleClient.cs
@@ -11,33 +11,48 @@ namespace Client
     {
         static int Main(string[] args)
         {
-            
-        String url = "https://harvest.grpc.apis.compassiot.cloud:443";
+
+        String url = "https://harvest-grpc.compassiot.cloud:443";
         String apiKey = "apikey bc8760b6f6f12b8ada9fdfd4503e847f";
         GrpcChannel channel = GrpcChannel.ForAddress(url);
         IngestService.IngestServiceClient client = new Compass.Harvest.V1Alpha1.IngestService.IngestServiceClient(channel);
         Grpc.Core.Metadata headers = new Grpc.Core.Metadata();
         headers.Add("Authorization", $"{apiKey}");
         Grpc.Core.Metadata header = new Grpc.Core.Metadata();
-        
-        PositionEvent evt = new PositionEvent { 
-            VehicleId = "testvehicle123", 
-            VehicleType = VehicleType.Car, 
-            TransportType = TransportType.Private, 
-            Acceleration =  new Acceleration {X = 1, Y = 2, Z = 3}, 
+
+        PartialEventLatLng partialEvt = new PartialEventLatLng {
+            VehicleId = "testvehicle123",
+            Region = "AUS",
+            Timestamp = DateTimeOffset.Now.ToUnixTimeSeconds(),
+            Longitude = 151.0,
+            Latitude = -33.0
+        };
+
+        PositionEvent evt = new PositionEvent {
+            VehicleId = "testvehicle123",
+            VehicleType = VehicleType.Car,
+            TransportType = TransportType.Private,
+            Acceleration =  new Acceleration {X = 1, Y = 2, Z = 3},
             Position = new Position{Latlng = new LatLng{Latitude = 45, Longitude = 90}},
             Gyro = new Gyro{Roll = -1, Pitch = -2, Yaw = -3},
-            Timestamp = Timestamp.FromDateTime(DateTime.UtcNow)};    
-        try 
+            Timestamp = Timestamp.FromDateTime(DateTime.UtcNow)};
+
+        BatchInsertRequest evts = new BatchInsertRequest {
+            Events = {evt}
+        };
+
+        try
         {
+            client.InsertPartialLatLng(partialEvt, headers);
             client.Insert(evt, headers);
+            client.BatchInsert(evts, headers);
         }
         catch (Exception e)
         {
             Console.Write(e.Message);
             return 0;
         }
-            Console.Write("\nSuccessfully pushed event via GRPC API!\n");
+            Console.Write("\nSuccessfully pushed events via GRPC API!\n");
             return 1;
         }
     }
--- streaming/java/src/main/java/compass/main/ExampleClient.java
+++ streaming/java/src/main/java/compass/main/ExampleClient.java
@@ -3,6 +3,7 @@ package compass.main;
 import com.google.protobuf.Timestamp;
 import compass.harvest.v1alpha1.IngestServiceGrpc;
 import compass.harvest.v1alpha1.PositionEventOuterClass.*;
+import compass.harvest.v1alpha1.Ingest.*;
 import compass.type.geo.Geo;
 import io.grpc.*;
 import io.grpc.stub.MetadataUtils;
@@ -11,7 +12,7 @@ public class ExampleClient {
 
     public static void main(String[] args) {
 
-        String url = "harvest.grpc.apis.compassiot.cloud";
+        String url = "harvest-grpc.compassiot.cloud";
         String apiKey = "apikey bc8760b6f6f12b8ada9fdfd4503e847f";
 
         Channel ch = ManagedChannelBuilder.forAddress(url, 443).build();
@@ -21,6 +22,19 @@ public class ExampleClient {
         Metadata.Key<String> key = Metadata.Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER);
         header.put(key, apiKey);
         client = MetadataUtils.attachHeaders(client, header);
+
+        // See below for more extensive example usage of the partial harvest api
+        // if you are NOT using the partial api and just using the start PositionEvent api
+        // you may ignore this
+        // https://github.com/compass-iot/harvest-api-examples/tree/main/streaming/curl
+        PartialEventLatLng partialLatLngEvt = PartialEventLatLng
+                .newBuilder()
+                .setVehicleId("abc123")
+                .setRegion("AUS")
+                .setTimestamp(System.currentTimeMillis() / 1000)
+                .setLatitude(-33.0)
+                .setLongitude(151.0)
+                .build();
 
         PositionEvent evt = PositionEvent
                 .newBuilder()
@@ -53,13 +67,19 @@ public class ExampleClient {
                 .putLabels("make", "toyota")
                 .putLabels("something", "nice")
                 .build();
+
+        BatchInsertRequest evts = BatchInsertRequest
+                .newBuilder()
+                .addEvents(evt)
+                .build();
         try {
             client.insert(evt);
+            client.batchInsert(evts);
+            client.insertPartialLatLng(partialLatLngEvt);
         } catch (Exception e) {
             System.err.println(e.getMessage());
             System.exit(1);
         }
-        System.out.println("Successfully pushed event via GRPC API!");
+        System.out.println("Successfully pushed events via GRPC API!");
     }
 }
-
